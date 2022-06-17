da305dab4d3149fb3ec4eb3bc562bd926296216b
suay-selay <suay_999@hotmail.com>
2022-06-17 14:06:33 +0400

    updating design of application

--- src/component/Map.jsx
+++ src/component/Map.jsx
@@ -1,54 +1,83 @@
 import React from 'react'
+import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
+
 
 const Map = () => {
 
-    const [position, setPosition] = React.useState({
-        lat: 0,
-        lng: 0
-    });
+    const [position, setPosition] = React.useState([40.3802948, 49.875192]);
 
-    const getPosition = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(position => {
-                setPosition({
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                });
-            });
-        } else {
-            console.log("Geolocation is not supported by this browser.");
-        }
+    const allPositions = [
+        position
+    ];
+
+    // const getCurrentPosition = () => {
+    //     if (navigator.geolocation) {
+    //         console.log(navigator.geolocation.getCurrentPosition(
+    //             position => {
+    //                 setPosition([position.coords.longitude, position.coords.latitude]);
+    //                 console.log(position.coords.latitude, position.coords.longitude);
+    //             }
+    //         ));
+    //     } else {
+    //         console.log('Geolocation is not supported in this browser');
+    //     }
+    // }
+
+    // getCurrentPosition();
+
+    const point = (x) => {
+        return (
+            <Marker position={x}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        );
     }
 
-    getPosition();
 
-    
 
     return (
-        <div>
+        <div className='container-fluid'>
             {/* card design with two values in bootstrap */}
 
-            <div className="card shadow-sm p-3 my-5 bg-body rounded">
+            <div className="card shadow-sm p-2 my-3 bg-body rounded">
                 <div className="card-body">
-                    <h5 className="card-title text-center">Map</h5>
+                    <h5 className="card-title text-center bg-light px-2 py-3 rounded">Current Position</h5>
                     <div className="card-text">
                         <div className="row">
                             <div className="col-md-6">
                                 <div className="form-group">
                                     <label htmlFor="exampleFormControlInput1">Latitude</label>
-                                    <input type="text" className="form-control" id="exampleFormControlInput1" value={position.lat} disabled />
+                                    <input type="text" className="form-control" id="exampleFormControlInput1" value={position[0]} disabled />
                                 </div>
                             </div>
                             <div className="col-md-6">
                                 <div className="form-group">
                                     <label htmlFor="exampleFormControlInput1">Longitude</label>
-                                    <input type="text" className="form-control" id="exampleFormControlInput1" value={position.lng} disabled />
+                                    <input type="text" className="form-control" id="exampleFormControlInput1" value={position[1]} disabled />
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div className="card shadow-sm p-2 my-3 bg-body rounded">
+                <h5 className="card-title text-center bg-light px-2 py-3 rounded">Open Street Map</h5>
+                <MapContainer center={position} zoom={15} scrollWheelZoom={false} style={{ height: '50vh', width: '100wh' }}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={position}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
+
         </div>
     )
 }
